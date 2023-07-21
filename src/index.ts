172b14152f4269706f8365b3c22ca642519e06f9
boseriddha <boseriddha@gmail.com>
2023-07-21 20:02:04 +0530

    setup server and mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello Typescript!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+app.listen(8000, () => {
+  console.log("Server running on http://localhost:8000/");
+});
+
+const MONGO_URL = process.env.MONGO_URL;
+
+if (!MONGO_URL) {
+  throw new Error("MongoDB URL is undefined. Cannot access .env file!\n");
+}
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => {
+  console.log(error);
+});
