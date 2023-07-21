e1091595dc267ddd5c912360383ef6c1c19502d5
boseriddha <boseriddha@gmail.com>
2023-07-22 00:05:32 +0530

    added delete user (2)

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,6 +3,31 @@ import { get, merge } from "lodash";
 
 import { getUserBySessionToken } from "../db/users";
 
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      console.log("You cannot delete other users!");
+      return res.sendStatus(403);
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 export const isAuthenticated = async (
   req: express.Request,
   res: express.Response,
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
-import { getAllUsers } from "../controllers/users";
-import { isAuthenticated } from "../middleware";
+import { getAllUsers, deleteUser } from "../controllers/users";
+import { isAuthenticated, isOwner } from "../middleware";
 
 export default (router: express.Router) => {
   router.get("/users", isAuthenticated, getAllUsers);
+  router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
 };
